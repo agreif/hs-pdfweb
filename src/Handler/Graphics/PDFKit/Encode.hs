429baff64453f5a17fbe441255e6140255b37d94
Alex Greif <alex.greif@gmail.com>
2018-12-26 15:28:10 +0100

    refactored modules

--- src/Handler/Graphics/PDFKit/Encode.hs
+++ src/Handler/Graphics/PDFKit/Encode.hs
@@ -4,7 +4,7 @@ module Handler.Graphics.PDFKit.Encode where
 
 import Import
 import qualified Data.ByteString.Char8 as B8
-import Handler.Graphics.PDFKit.Types
+import Handler.Graphics.PDFKit.Pdf
 
 
 encodePdf :: PdfDocument -> ByteString
--- src/Handler/Graphics/PDFKit/Pdf.hs
+++ src/Handler/Graphics/PDFKit/Pdf.hs
@@ -1,13 +1,12 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-module Handler.Graphics.PDFKit.Types where
+module Handler.Graphics.PDFKit.Pdf where
 
 import Import
 import qualified Data.List as L
 import qualified Data.Text as T
 import qualified Data.ByteString.Char8 as B8
 import Handler.Graphics.PDFKit.Helpers
-
 
 data PdfDocument = PdfDocument
   { pdfDocumentVersion :: Text
@@ -264,24 +263,89 @@ instance ToJSON PdfTrailer where
 
 -----------------------------------------------
 
-data Action =
-  ActionInfoSetup
-  | ActionInfoSetProducer Text
-  | ActionInfoSetCreator Text
-  | ActionFinalize
-  | ActionFont
-  | ActionResources
-  | ActionPage
-  | ActionPageSetSize PdfPageSize
-  | ActionPageSetLayout PdfPageLayout
-  | ActionPageSetMargin Double
-  | ActionPageSetMargins Double Double Double Double
-  | ActionPageSetSizeCustom Double Double
+data PdfPageMargins =
+  PdfPageMargins
+  { pdfPageMarginTop :: Double
+  , pdfPageMarginLeft :: Double
+  , pdfPageMarginBottom :: Double
+  , pdfPageMarginRight :: Double
+  }
 
-build :: Action -> PdfBuilder
-build action = PdfBuilderM () [action]
+defaultPageMargins :: PdfPageMargins
+defaultPageMargins = PdfPageMargins 72 72 72 72
+
+instance ToJSON PdfPageMargins where
+  toJSON o = object
+    [ "top" .= pdfPageMarginTop o
+    , "left" .= pdfPageMarginLeft o
+    , "bottom" .= pdfPageMarginBottom o
+    , "right" .= pdfPageMarginRight o
+    ]
 
 -----------------------------------------------
+
+data PdfPageLayout = Portrait | Landscape
+  deriving Show
+
+portrait :: PdfPageLayout
+portrait = Portrait
+
+landscape :: PdfPageLayout
+landscape = Landscape
+
+-----------------------------------------------
+
+data PdfPageSize = PdfPageSize
+  { pdfPageSizeWidth :: Double
+  , pdfPageSizeHeight :: Double
+  }
+
+instance ToJSON PdfPageSize where
+  toJSON o = object
+    [ "width" .= pdfPageSizeWidth o
+    , "height" .= pdfPageSizeHeight o
+    ]
+
+-----------------------------------------------
+
+class ToByteStringLines b where
+  toByteStringLines :: b -> PdfDocument -> [ByteString]
+
+class IsExecutableAction a where
+  execute :: a -> PdfDocument -> PdfDocument
+
+data PdfBuilderM b = PdfBuilderM b [Action]
+
+type PdfBuilder = PdfBuilderM ()
+
+instance Functor PdfBuilderM where
+  fmap = liftM
+
+instance Applicative PdfBuilderM where
+  pure  = return
+  (<*>) = ap
+
+instance Monad PdfBuilderM where
+  return b = PdfBuilderM b []
+  PdfBuilderM b actions1 >>= f =
+    let PdfBuilderM b2 actions2 = f b
+    in  PdfBuilderM b2 (actions1 ++ actions2)
+
+-----------------------------------------------
+
+pdfPagesTuple :: PdfDocument -> (PdfPages, [PdfPage], PdfPage)
+pdfPagesTuple pdfDoc = (pdfPages, initPages, lastPage)
+  where
+    pdfPages = pdfDocumentPages pdfDoc
+    initPages = L.init $ pdfPagesKids pdfPages
+    lastPage = L.last $ pdfPagesKids pdfPages
+
+applyLayout :: PdfPageSize -> PdfPageLayout -> PdfPageSize
+applyLayout size Portrait = size
+applyLayout (PdfPageSize {pdfPageSizeWidth = w, pdfPageSizeHeight = h }) Landscape =
+  PdfPageSize { pdfPageSizeWidth = h
+              , pdfPageSizeHeight = w
+              }
 
 pdfDocumentByteStringLineBlocks :: PdfDocument -> ([ByteString], [[ByteString]], [ByteString])
 pdfDocumentByteStringLineBlocks pdfDoc =
@@ -313,6 +377,128 @@ pdfDocumentByteStringLineBlocks pdfDoc =
        , encodeUtf8 "%%EOF"
        ]
   )
+
+size4A0 :: PdfPageSize
+size4A0 = PdfPageSize 4767.87 6740.79
+size2A0 :: PdfPageSize
+size2A0 = PdfPageSize 3370.39 4767.87
+sizeA0 :: PdfPageSize
+sizeA0 = PdfPageSize 2383.94 3370.39
+sizeA1 :: PdfPageSize
+sizeA1 = PdfPageSize 1683.78 2383.94
+sizeA2 :: PdfPageSize
+sizeA2 = PdfPageSize 1190.55 1683.78
+sizeA3 :: PdfPageSize
+sizeA3 = PdfPageSize 841.89 1190.55
+sizeA4 :: PdfPageSize
+sizeA4 = PdfPageSize 595.28 841.89
+sizeA5 :: PdfPageSize
+sizeA5 = PdfPageSize 419.53 595.28
+sizeA6 :: PdfPageSize
+sizeA6 = PdfPageSize 297.64 419.53
+sizeA7 :: PdfPageSize
+sizeA7 = PdfPageSize 209.76 297.64
+sizeA8 :: PdfPageSize
+sizeA8 = PdfPageSize 147.40 209.76
+sizeA9 :: PdfPageSize
+sizeA9 = PdfPageSize 104.88 147.40
+sizeA10 :: PdfPageSize
+sizeA10 = PdfPageSize 73.70 104.88
+sizeB0 :: PdfPageSize
+sizeB0 = PdfPageSize 2834.65 4008.19
+sizeB1 :: PdfPageSize
+sizeB1 = PdfPageSize 2004.09 2834.65
+sizeB2 :: PdfPageSize
+sizeB2 = PdfPageSize 1417.32 2004.09
+sizeB3 :: PdfPageSize
+sizeB3 = PdfPageSize 1000.63 1417.32
+sizeB4 :: PdfPageSize
+sizeB4 = PdfPageSize 708.66 1000.63
+sizeB5 :: PdfPageSize
+sizeB5 = PdfPageSize 498.90 708.66
+sizeB6 :: PdfPageSize
+sizeB6 = PdfPageSize 354.33 498.90
+sizeB7 :: PdfPageSize
+sizeB7 = PdfPageSize 249.45 354.33
+sizeB8 :: PdfPageSize
+sizeB8 = PdfPageSize 175.75 249.45
+sizeB9 :: PdfPageSize
+sizeB9 = PdfPageSize 124.72 175.75
+sizeB10 :: PdfPageSize
+sizeB10 = PdfPageSize 87.87 124.72
+sizeC0 :: PdfPageSize
+sizeC0 = PdfPageSize 2599.37 3676.54
+sizeC1 :: PdfPageSize
+sizeC1 = PdfPageSize 1836.85 2599.37
+sizeC2 :: PdfPageSize
+sizeC2 = PdfPageSize 1298.27 1836.85
+sizeC3 :: PdfPageSize
+sizeC3 = PdfPageSize 918.43 1298.27
+sizeC4 :: PdfPageSize
+sizeC4 = PdfPageSize 649.13 918.43
+sizeC5 :: PdfPageSize
+sizeC5 = PdfPageSize 459.21 649.13
+sizeC6 :: PdfPageSize
+sizeC6 = PdfPageSize 323.15 459.21
+sizeC7 :: PdfPageSize
+sizeC7 = PdfPageSize 229.61 323.15
+sizeC8 :: PdfPageSize
+sizeC8 = PdfPageSize 161.57 229.61
+sizeC9 :: PdfPageSize
+sizeC9 = PdfPageSize 113.39 161.57
+sizeC10 :: PdfPageSize
+sizeC10 = PdfPageSize 79.37 113.39
+sizeRA0 :: PdfPageSize
+sizeRA0 = PdfPageSize 2437.80 3458.27
+sizeRA1 :: PdfPageSize
+sizeRA1 = PdfPageSize 1729.13 2437.80
+sizeRA2 :: PdfPageSize
+sizeRA2 = PdfPageSize 1218.90 1729.13
+sizeRA3 :: PdfPageSize
+sizeRA3 = PdfPageSize 864.57 1218.90
+sizeRA4 :: PdfPageSize
+sizeRA4 = PdfPageSize 609.45 864.57
+sizeSRA0 :: PdfPageSize
+sizeSRA0 = PdfPageSize 2551.18 3628.35
+sizeSRA1 :: PdfPageSize
+sizeSRA1 = PdfPageSize 1814.17 2551.18
+sizeSRA2 :: PdfPageSize
+sizeSRA2 = PdfPageSize 1275.59 1814.17
+sizeSRA3 :: PdfPageSize
+sizeSRA3 = PdfPageSize 907.09 1275.59
+sizeSRA4 :: PdfPageSize
+sizeSRA4 = PdfPageSize 637.80 907.09
+sizeEXECUTIVE :: PdfPageSize
+sizeEXECUTIVE = PdfPageSize 521.86 756.00
+sizeFOLIO :: PdfPageSize
+sizeFOLIO = PdfPageSize 612.00 936.00
+sizeLEGAL :: PdfPageSize
+sizeLEGAL = PdfPageSize 612.00 1008.00
+sizeLETTER :: PdfPageSize
+sizeLETTER = PdfPageSize 612.00 792.00
+sizeTABLOID :: PdfPageSize
+sizeTABLOID = PdfPageSize 792.00 1224.00
+
+-----------------------------------------------
+
+data Action =
+  ActionInfoSetup
+  | ActionInfoSetProducer Text
+  | ActionInfoSetCreator Text
+  | ActionFinalize
+  | ActionFont
+  | ActionResources
+  | ActionPage
+  | ActionPageSetSize PdfPageSize
+  | ActionPageSetLayout PdfPageLayout
+  | ActionPageSetMargin Double
+  | ActionPageSetMargins Double Double Double Double
+  | ActionPageSetSizeCustom Double Double
+
+build :: Action -> PdfBuilder
+build action = PdfBuilderM () [action]
+
+-----------------------------------------------
 
 instance IsExecutableAction Action where
   execute ActionInfoSetup pdfDoc =
@@ -504,186 +690,3 @@ instance IsExecutableAction Action where
     }
     where
       (pdfPages, initPages, lastPage) = pdfPagesTuple pdfDoc
-
------------------------------------------------
-
-defaultPageMargins :: PdfPageMargins
-defaultPageMargins = PdfPageMargins 72 72 72 72
-
-data PdfPageMargins =
-  PdfPageMargins
-  { pdfPageMarginTop :: Double
-  , pdfPageMarginLeft :: Double
-  , pdfPageMarginBottom :: Double
-  , pdfPageMarginRight :: Double
-  }
-
-instance ToJSON PdfPageMargins where
-  toJSON o = object
-    [ "top" .= pdfPageMarginTop o
-    , "left" .= pdfPageMarginLeft o
-    , "bottom" .= pdfPageMarginBottom o
-    , "right" .= pdfPageMarginRight o
-    ]
-
-data PdfPageLayout = Portrait | Landscape
-  deriving Show
-
-portrait :: PdfPageLayout
-portrait = Portrait
-
-landscape :: PdfPageLayout
-landscape = Landscape
-
-applyLayout :: PdfPageSize -> PdfPageLayout -> PdfPageSize
-applyLayout size Portrait = size
-applyLayout (PdfPageSize {pdfPageSizeWidth = w, pdfPageSizeHeight = h }) Landscape =
-  PdfPageSize { pdfPageSizeWidth = h
-              , pdfPageSizeHeight = w
-              }
-
-data PdfPageSize = PdfPageSize
-  { pdfPageSizeWidth :: Double
-  , pdfPageSizeHeight :: Double
-  }
-
-instance ToJSON PdfPageSize where
-  toJSON o = object
-    [ "width" .= pdfPageSizeWidth o
-    , "height" .= pdfPageSizeHeight o
-    ]
-
-size4A0 :: PdfPageSize
-size4A0 = PdfPageSize 4767.87 6740.79
-size2A0 :: PdfPageSize
-size2A0 = PdfPageSize 3370.39 4767.87
-sizeA0 :: PdfPageSize
-sizeA0 = PdfPageSize 2383.94 3370.39
-sizeA1 :: PdfPageSize
-sizeA1 = PdfPageSize 1683.78 2383.94
-sizeA2 :: PdfPageSize
-sizeA2 = PdfPageSize 1190.55 1683.78
-sizeA3 :: PdfPageSize
-sizeA3 = PdfPageSize 841.89 1190.55
-sizeA4 :: PdfPageSize
-sizeA4 = PdfPageSize 595.28 841.89
-sizeA5 :: PdfPageSize
-sizeA5 = PdfPageSize 419.53 595.28
-sizeA6 :: PdfPageSize
-sizeA6 = PdfPageSize 297.64 419.53
-sizeA7 :: PdfPageSize
-sizeA7 = PdfPageSize 209.76 297.64
-sizeA8 :: PdfPageSize
-sizeA8 = PdfPageSize 147.40 209.76
-sizeA9 :: PdfPageSize
-sizeA9 = PdfPageSize 104.88 147.40
-sizeA10 :: PdfPageSize
-sizeA10 = PdfPageSize 73.70 104.88
-sizeB0 :: PdfPageSize
-sizeB0 = PdfPageSize 2834.65 4008.19
-sizeB1 :: PdfPageSize
-sizeB1 = PdfPageSize 2004.09 2834.65
-sizeB2 :: PdfPageSize
-sizeB2 = PdfPageSize 1417.32 2004.09
-sizeB3 :: PdfPageSize
-sizeB3 = PdfPageSize 1000.63 1417.32
-sizeB4 :: PdfPageSize
-sizeB4 = PdfPageSize 708.66 1000.63
-sizeB5 :: PdfPageSize
-sizeB5 = PdfPageSize 498.90 708.66
-sizeB6 :: PdfPageSize
-sizeB6 = PdfPageSize 354.33 498.90
-sizeB7 :: PdfPageSize
-sizeB7 = PdfPageSize 249.45 354.33
-sizeB8 :: PdfPageSize
-sizeB8 = PdfPageSize 175.75 249.45
-sizeB9 :: PdfPageSize
-sizeB9 = PdfPageSize 124.72 175.75
-sizeB10 :: PdfPageSize
-sizeB10 = PdfPageSize 87.87 124.72
-sizeC0 :: PdfPageSize
-sizeC0 = PdfPageSize 2599.37 3676.54
-sizeC1 :: PdfPageSize
-sizeC1 = PdfPageSize 1836.85 2599.37
-sizeC2 :: PdfPageSize
-sizeC2 = PdfPageSize 1298.27 1836.85
-sizeC3 :: PdfPageSize
-sizeC3 = PdfPageSize 918.43 1298.27
-sizeC4 :: PdfPageSize
-sizeC4 = PdfPageSize 649.13 918.43
-sizeC5 :: PdfPageSize
-sizeC5 = PdfPageSize 459.21 649.13
-sizeC6 :: PdfPageSize
-sizeC6 = PdfPageSize 323.15 459.21
-sizeC7 :: PdfPageSize
-sizeC7 = PdfPageSize 229.61 323.15
-sizeC8 :: PdfPageSize
-sizeC8 = PdfPageSize 161.57 229.61
-sizeC9 :: PdfPageSize
-sizeC9 = PdfPageSize 113.39 161.57
-sizeC10 :: PdfPageSize
-sizeC10 = PdfPageSize 79.37 113.39
-sizeRA0 :: PdfPageSize
-sizeRA0 = PdfPageSize 2437.80 3458.27
-sizeRA1 :: PdfPageSize
-sizeRA1 = PdfPageSize 1729.13 2437.80
-sizeRA2 :: PdfPageSize
-sizeRA2 = PdfPageSize 1218.90 1729.13
-sizeRA3 :: PdfPageSize
-sizeRA3 = PdfPageSize 864.57 1218.90
-sizeRA4 :: PdfPageSize
-sizeRA4 = PdfPageSize 609.45 864.57
-sizeSRA0 :: PdfPageSize
-sizeSRA0 = PdfPageSize 2551.18 3628.35
-sizeSRA1 :: PdfPageSize
-sizeSRA1 = PdfPageSize 1814.17 2551.18
-sizeSRA2 :: PdfPageSize
-sizeSRA2 = PdfPageSize 1275.59 1814.17
-sizeSRA3 :: PdfPageSize
-sizeSRA3 = PdfPageSize 907.09 1275.59
-sizeSRA4 :: PdfPageSize
-sizeSRA4 = PdfPageSize 637.80 907.09
-sizeEXECUTIVE :: PdfPageSize
-sizeEXECUTIVE = PdfPageSize 521.86 756.00
-sizeFOLIO :: PdfPageSize
-sizeFOLIO = PdfPageSize 612.00 936.00
-sizeLEGAL :: PdfPageSize
-sizeLEGAL = PdfPageSize 612.00 1008.00
-sizeLETTER :: PdfPageSize
-sizeLETTER = PdfPageSize 612.00 792.00
-sizeTABLOID :: PdfPageSize
-sizeTABLOID = PdfPageSize 792.00 1224.00
-
------------------------------------------------
-
-pdfPagesTuple :: PdfDocument -> (PdfPages, [PdfPage], PdfPage)
-pdfPagesTuple pdfDoc = (pdfPages, initPages, lastPage)
-  where
-    pdfPages = pdfDocumentPages pdfDoc
-    initPages = L.init $ pdfPagesKids pdfPages
-    lastPage = L.last $ pdfPagesKids pdfPages
-
------------------------------------------------
-
-class ToByteStringLines b where
-  toByteStringLines :: b -> PdfDocument -> [ByteString]
-
-class IsExecutableAction a where
-  execute :: a -> PdfDocument -> PdfDocument
-
-data PdfBuilderM b = PdfBuilderM b [Action]
-
-type PdfBuilder = PdfBuilderM ()
-
-instance Functor PdfBuilderM where
-  fmap = liftM
-
-instance Applicative PdfBuilderM where
-  pure  = return
-  (<*>) = ap
-
-instance Monad PdfBuilderM where
-  return b = PdfBuilderM b []
-  PdfBuilderM b actions1 >>= f =
-    let PdfBuilderM b2 actions2 = f b
-    in  PdfBuilderM b2 (actions1 ++ actions2)
