6480b17abbc04cfce9cdb2fff8aca545a698b2d8
Alex Greif <alex.greif@gmail.com>
2019-01-06 16:47:00 +0100

    doc

--- src/Handler/SamplePdf.hs
+++ src/Handler/SamplePdf.hs
@@ -24,22 +24,26 @@ samplePdfDoc = do
           linePoint 200 200
           lineStroke
         text $ do
+          textPos 180 190
+          textColorCmyk 10 60 60 80
+          textFillOpacity 1
           font helvetica
-          fontSize 12
+          fontSize 24
           content "foo bar"
-        textHelvetica24 $ do
-          content "ü ä=ã, ö=õ, ü=ũ"
+        textHelvetica24BlueOpaque $ do content "ü ä=ã, ö=õ, ü=ũ"
   where
     pageA4Landscape :: PdfPageBuilder -> PdfDocumentBuilder
     pageA4Landscape =
       pageTemplate $ do
         pageSize sLetter
         layout landscape
-    textHelvetica24 :: PdfTextBuilder -> PdfPageBuilder
-    textHelvetica24 =
+    textHelvetica24BlueOpaque :: PdfTextBuilder -> PdfPageBuilder
+    textHelvetica24BlueOpaque =
       textTemplate $ do
         font helvetica
         fontSize 24
+        textColorRgb 0 0 255
+        textFillOpacity 0.5
 
 getSamplePdfJsonR :: Handler Value
 getSamplePdfJsonR = do
